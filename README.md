7c789181576da33ccf47ccfb2687cea56bebbaf6
Kamas "Iceberg" Lau <kamaslau@outlook.com>
2017-05-15 11:43:00 +0800

    beta20170515
    【新增】通过jQuery以AJAX方式上传单文件/多文件到服务器端，支持多种服务器端开发语言（包括但不限于Java、NodeJS、PHP、Rub
    y、.net等）；为便于开发，示例中已兼容了BootStrap3.3.7。

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # ajaxupload
-通过AJAX上传单文件/多文件，示例兼容了BootStrap
+
+通过jQuery以AJAX方式上传单文件/多文件到服务器端，支持多种服务器端开发语言（包括但不限于Java、NodeJS、PHP、Ruby、.net等）；为便于开发，示例中已兼容了BootStrap3.3.7。
+
+## 环境需求
+
+* jQuery2+，及所需环境（推荐jQuery3）
+* 支持AJAX
+* 可写的服务器端图片存储文件夹，即图片存储根目录
+
+## WEB前端（以外链JavaScript方式为例）
+
+* 将“ajaxupload.js”文件中“api_url”修改为服务器端处理上传的类方法URL、“uploads_url”修改为图片存储根目录
+* 将“ajaxupload.js”文件放入项目的JavaScript资源目录
+* 将“client.html”文件中button的data-target-dir属性修改为需要保存待上传图片的子目录名称（下称目标文件夹）
+* 将“client.html”文件中示例代码整合入项目对应的视图文件，相应修改外链JavaScript文件的URL
+
+## 服务器端处理流程
+
+1. 创建“status”（int）、“content”（string）、“items”（array/list，视具体服务器端语言而定，下同）类属性；
+2. 通过$_GET['target']获取目标文件夹；
+3. 通过$_FILES获取并依次处理待上传文件;
+4. 创建单个文件处理结果数组，含有单个文件上传结果status（int）键值对及单个文件上传详情content（int/string/array/list）键值对；
+5. 若文件上传成功，赋值status数组键值为200，否则为400；
+6. 若文件上传成功，赋值content数组键值为保存后文件的URL（相对或绝对均可，视具体需要而定），否则赋值content为实际需要的返回信息；
+7. 将单个文件处理结果数组推入“items”类属性数组；
+8. 若所有文件均上传成功，赋值status类属性为200，否则为400；
+9. 若所有文件文件上传失败，赋值content类属性为“上传成功”，否则赋值content为实际需要的返回信息；
+10. 通过json格式将所有类属性键值对返回（在不影响上述流程的前提下，可根据实际需要增加其它返回内容）。
+
+## 服务器端响应示例
+
+中文内容已转码以便阅读。
+
+```JavaScript
+{
+	"status":400,
+	"content":"部分上传成功",
+	"items":[
+		{"status":200,"content":"20170515_110640.png"},
+		{"status":200,"content":"20170515_1106401.png"},
+		{"status":200,"content":"20170515_1106402.png"},
+		{
+			"status":400,
+			"content":{
+				"file":{
+					"name":"1227650_all.sql",
+					"type":"application\/sql",
+					"tmp_name":"\/tmp\/phpOdFKZD",
+					"error":0,
+					"size":39819
+				},
+				"descirption":"不可上传该类型的文件"
+			}
+		}
+	],
+	"param":{
+		"get":{
+			"target":"project"
+		},
+		"post":[]
+	},
+	"elapsed_time":"0.0246 s"
+}
+```
---
+++ file-upload.js
@@ -0,0 +1,154 @@
+/**
+ * Ajaxupload类
+ *
+ * 处理AJAX文件上传
+ *
+ * @version beta20170515
+ * @author Kamas 'Iceberg' Lau <https://github.com/kamaslau/ajaxupload>
+ * @copyright Kamas 'Iceberg' Lau <kamaslau@outlook.com>
+ */
+
+// AJAX文件上传服务器端URL；上传目标文件夹名稍后通过上传按钮的相关属性获取
+var api_url = '//www.doc4apis.com/ajaxupload?target=';
+
+// 图片存储根路径
+var uploads_url = '//www.doc4apis.com/uploads/';
+
+/* 从此处起请谨慎修改 */
+$(function(){
+	// 文件上传主处理方法
+	$('.file-upload').click(function(){
+		// 检查当前浏览器是否支持AJAX文件上传
+		check_support_formdata();
+
+		// 禁用上传按钮
+		$(this).attr('disabled', 'disabled');
+		$(this).html('<i class="fa fa-refresh" aria-hidden=true></i> 上传中……');
+
+		// 处理上传
+		var button = $(this);
+		file_upload( button );
+
+		// 激活上传按钮
+		button.removeAttr('disabled');
+		button.html('<i class="fa fa-upload" aria-hidden=true></i> 上传');
+	});
+
+	// 检查浏览器是否支持完成文件上传必须的XHR2（FormData）功能
+	function check_support_formdata()
+	{
+		if ( ! window.hasOwnProperty('FormData') )
+		{
+			alert('您正在使用安全性差或者已过时的浏览器；请使用谷歌或火狐浏览器。');
+			return false;
+		}
+	}
+
+	// 获取文件大小
+	function file_size(file)
+	{
+		return (file.files[0].size / 1024).toFixed(2);
+	}
+
+	// 处理文件上传
+	function file_upload(button)
+	{
+		// 创建FormData对象
+		var formData = new FormData();
+
+		// 获取文件选择器对象
+		var file_selector = $( '#' + button.attr('data-selector-id') );
+
+		// 获取待上传的文件数量（HTML中可通过type=file表单项中添加multiple属性对多文件上传提供支持）
+		var file_count = file_selector[0].files.length;
+		
+		// 若无任何文件被选中，进行提示
+		if (file_count == 0)
+		{
+			alert('请选择文件');
+			return;
+		}
+
+		// 将所有需上传的文件信息放入formData对象
+		for (var i=0; i<file_count; i++)
+		{
+			formData.append('file'+i, file_selector[0].files[ i ] );
+		}
+
+		// 获取上传目标文件夹名
+		var dir_target = button.attr('data-target-dir');
+
+		$.ajax({
+	        url: api_url + dir_target, // 处理上传的后端URL
+	        type: 'POST',
+			cache: false, // 上传文件不需要缓存
+	        data: formData,
+
+	        processData: false,  // 不处理发送的数据
+	        contentType: false // 不设置Content-Type请求头
+	    }).then(function(data){
+
+			// 进行总体提示
+			if (data.status == 200)
+			{
+				alert('成功上传');
+			}
+			else // 若上传失败，进行提示
+			{
+				alert(data.content);
+			}
+
+			// 初始化表单值
+			var input_value = '';
+
+			// 轮番显示上传结果
+			$.each(
+				data.items,
+				function(i, item)
+				{
+					// 若上传成功，显示预览；若上传失败，显示源文件信息及错误描述
+					if (item.status == 200)
+					{
+						// 更新预览区
+						var item_content =
+						'<li class="col-xs-12 col-md-3">' +
+						'	<figure class="thumbnail">' +
+						'		<figcaption>' + item.content + '</figcaption>' +
+						'		<img src="' + uploads_url + dir_target + '/'+ item.content +'">' +
+						'	</figure>' +
+						'</li>';
+
+						// 更新表单值
+						input_value += item.content + ' ';
+					}
+					else
+					{
+						// 更新预览区
+						var item_content =
+						'<li class="col-xs-12 col-md-3">' +
+						'	<dl>' +
+						'		<dt>失败原因</dt><dd>' + item.content.descirption + '</dd>' +
+						'		<dt>源文件名</dt><dd>' + item.content.file.name + '</dd>' +
+						'		<dt>源文件类型</dt><dd>' + item.content.file.type + '</dd>' +
+						'		<dt>源文件大小</dt><dd>' + (item.content.file.size / 1024).toFixed(2) + 'kb</dd>' +
+						'	</dl>' +
+						'</li>';
+					}
+
+					// 在相应位置显示预览
+					var file_previewer = button.siblings('.upload_preview');
+					file_previewer.prepend(item_content);
+				}
+			); //end $.each
+
+			// 向相应表单项赋值
+			input_value = $.trim(input_value);
+			if (input_value != '')
+			{
+				$('[name=' + button.attr('data-input-name') + ']').val(input_value);
+			}
+	    });
+
+	} //end file_upload
+
+});
